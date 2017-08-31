b387f334f88145c8ba8cc1bd3c0eaee65ee45ff1
jonathanholt <jonathanholt1989@gmail.com>
2017-08-31 23:20:01 +0100

    Core website files commit

--- src/com/jonathanholt/parkrun/ParkRunAddScriptServlet.java
+++ src/com/jonathanholt/parkrun/ParkRunAddScriptServlet.java
@@ -3,12 +3,16 @@ package com.jonathanholt.parkrun;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
+import java.io.UnsupportedEncodingException;
 
 import javax.servlet.http.*;
 import java.net.URL;
+import java.net.URLDecoder;
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.LinkedHashMap;
 import java.util.List;
+import java.util.Map;
 
 
 @SuppressWarnings("serial")
@@ -36,7 +40,7 @@ public class ParkRunAddScriptServlet extends HttpServlet {
 		   
 		   //String json = new Gson().toJson(parkruns);
 		
-		URL url = this.getServletContext().getResource("/tester.csv");
+		URL url = this.getServletContext().getResource("/parkruns.csv");
 		System.out.println(url);
 		InputStreamReader isr = new InputStreamReader(url.openStream());
 		BufferedReader reader = new BufferedReader(isr);
@@ -47,18 +51,46 @@ public class ParkRunAddScriptServlet extends HttpServlet {
 			List<String> elephantList = Arrays.asList(line.split(","));
 			if(elephantList.size() > 2){
 			System.out.println(elephantList.get(0));
-			System.out.println(elephantList.get(1));
-			System.out.println(elephantList.get(2));
-			System.out.println(elephantList.get(3));
-			//Parkrun toAdd = new Parkrun(elephantList.get(0), "null", elephantList.get(1), "null", elephantList.get(2), elephantList.get(3));
-			dc.doAdd(elephantList.get(2), "null", "null", elephantList.get(3), elephantList.get(4));	    
-			System.out.println(elephantList.get(0)+" "+elephantList.get(1)+" "+elephantList.get(2)+" "+elephantList.get(3)+" "+elephantList.get(4));
+			//System.out.println(elephantList.get(1));
+			
+			URL url2 = new URL(elephantList.get(1));
+			Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+		    String query = url2.getQuery();
+		    String[] pairs = query.split("&");
+		    for (String pair : pairs) {
+		        int idx = pair.indexOf("=");
+		        query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+		    }
+		    String ll = query_pairs.get("ll"); 
+		    String[] llll = ll.split(",");
+		    String latitude = llll[0];
+		    String longitude = llll[1];
+		    System.out.println("Lat = "+latitude+" Long = "+longitude);
+		    	
+		    //System.out.println(ll);
+		    
+			//System.out.println(elephantList.get(2));
+			//System.out.println(elephantList.get(3));
+			Parkrun toAdd = new Parkrun(elephantList.get(0), "null", "null", "null", latitude, longitude);
+			//dc.doAdd(elephantList.get(0), "null", "null", latitude, longitude);	    
+			//System.out.println(elephantList.get(0)+" "+elephantList.get(1)+" "+elephantList.get(2)+" "+elephantList.get(3)+" "+elephantList.get(4));
 			}
 			} 
 		   
-		   resp.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
-		   resp.setCharacterEncoding("UTF-8"); // You want world domination, huh?
+		   //resp.setContentType("text/plain");  // Set content type of the response so that jQuery knows what it can expect.
+		   //resp.setCharacterEncoding("UTF-8"); // You want world domination, huh?
 		  // resp.getWriter().print(json);       // Write response body.
+	}
+	
+	public static Map<String, String> splitQuery(URL url) throws UnsupportedEncodingException {
+	    Map<String, String> query_pairs = new LinkedHashMap<String, String>();
+	    String query = url.getQuery();
+	    String[] pairs = query.split("&");
+	    for (String pair : pairs) {
+	        int idx = pair.indexOf("=");
+	        query_pairs.put(URLDecoder.decode(pair.substring(0, idx), "UTF-8"), URLDecoder.decode(pair.substring(idx + 1), "UTF-8"));
+	    }
+	    return query_pairs;
 	}
 	
 	public void doGetUserInput(){
